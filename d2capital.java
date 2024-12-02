e1c3093ba3138fe074f04588aafa1bac71d72efc
Somnath474 <somnathgupta474@gmail.com>
2024-12-03 00:04:54 +0530

    Add files via upload

---
+++ d2capital.java
@@ -0,0 +1,24 @@
+// public class d2capital {
+//     public static String upper(String str) {
+//         StringBuilder sb=new StringBuilder("");
+//         char ch=Character.upper(str.charAt(0));
+//         sb.append(ch);
+
+//         for(int i=1;i<str.length();i++){
+//             if(str.charAt(i))==' ' && i<str.length()-1{
+//                 sb.append(str.charAt(i));
+//                 i++;
+//                 sb.append(Character.upper(str.charAt(i)));
+//             }
+//             else{
+//                 sb.append(str.charAt(i));
+//             }
+//         }
+//         return sb.toString();
+//     }
+
+//     public static void main(String[] args) {
+//         String str="hi,i am shradha";
+//         System.out.println(upper(str));
+//     }
+// }
